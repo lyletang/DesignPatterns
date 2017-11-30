f9c172d9460c2e239601b692b8b551ad448d6fa1
lyletang <lyletang@qq.com>
2017-11-30 16:50:59 +0800

    factory-readme commit

--- 创建型模式/工厂模式/abstract_factory.py
+++ 创建型模式/工厂模式/abstract_factory.py
@@ -73,7 +73,7 @@ class WizardWorld:
 
     def __str__(self):
         return '\n\n\t----- Wizard World -----'
-    
+   
     def make_character(self):
         return Wizard(self.player_name)
     
